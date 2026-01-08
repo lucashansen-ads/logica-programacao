071d378841f08222e45c50ae77fff658d2f5a637
lucashansen-ads <lucashansenwork.job@gmail.com>
2026-01-08 00:18:41 -0300

    Implement even or odd number check
    Add algorithm to check if a number is even or odd

---
+++ exercicio1.js
@@ -0,0 +1,11 @@
+Numero par ou impar
+Objetivo: Criar um algoritmo qie verifique se um n umero é par ou impar
+
+let numero = 10;
+IF (NUMERO % 2 ===0) {
+    console.log("O numero é par");
+} else { 
+     console.log("O numero é impar");
+} 
+
+// se o resto da divisão por 2 for igual a 0, o numero é par
