90e405199f1208307b500c6de9c87bf77b15bfe5
lucashansen-ads <lucashansenwork.job@gmail.com>
2026-01-08 01:29:08 -0300

    Create exercicio3.js

---
+++ exercicio3.js
@@ -0,0 +1,8 @@
+// Exercicio 3 - Tabuada 
+// Objetivo: mostrar tabuada de 1 a 10
+
+let numero = 5
+
+for(let i = 1; i <= 10; i++) { 
+    console.log(numero + " x " + i + " = " + (numero * i ));
+} 
